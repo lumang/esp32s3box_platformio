116e489e8c01537852bdeb07fce82cee3c2daf53
guannan007 <lu_mang@yeah.net>
2024-07-14 02:25:10 +0800

    create b watch

---
+++ src/app_hal.cpp
@@ -0,0 +1,174 @@
+#include "app_hal.h"
+#include <ESP_Panel_Library.h>
+#include <ESP_IOExpander_Library.h>
+#include <lvgl.h>
+#include "../ui/ui.h"
+#include <Arduino.h>
+#include "app_hal.h"
+ESP_Panel *panel = NULL;
+SemaphoreHandle_t lvgl_mux = NULL;                  // LVGL mutex
+LV_IMG_DECLARE(kapiWorking_gif);
+lv_obj_t * img_gif;
+
+void ui_gif_init()
+{
+  img_gif = lv_gif_create(ui_Screen1);
+  lv_gif_set_src(img_gif, &kapiWorking_gif);
+  lv_obj_align(img_gif, LV_ALIGN_CENTER, 0, 40);
+}
+
+#if ESP_PANEL_LCD_BUS_TYPE == ESP_PANEL_BUS_TYPE_RGB
+/* Display flushing */
+void lvgl_port_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+    panel->getLcd()->drawBitmap(area->x1, area->y1, area->x2 + 1, area->y2 + 1, color_p);
+    lv_disp_flush_ready(disp);
+}
+#else
+/* Display flushing */
+void lvgl_port_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+    panel->getLcd()->drawBitmap(area->x1, area->y1, area->x2 + 1, area->y2 + 1, color_p);
+}
+
+bool notify_lvgl_flush_ready(void *user_ctx)
+{
+    lv_disp_drv_t *disp_driver = (lv_disp_drv_t *)user_ctx;
+    lv_disp_flush_ready(disp_driver);
+    return false;
+}
+#endif /* ESP_PANEL_LCD_BUS_TYPE */
+
+#if ESP_PANEL_USE_LCD_TOUCH
+/* Read the touchpad */
+void lvgl_port_tp_read(lv_indev_drv_t * indev, lv_indev_data_t * data)
+{
+    panel->getLcdTouch()->readData();
+
+    bool touched = panel->getLcdTouch()->getTouchState();
+    if(!touched) {
+        data->state = LV_INDEV_STATE_REL;
+    } else {
+        TouchPoint point = panel->getLcdTouch()->getPoint();
+
+        data->state = LV_INDEV_STATE_PR;
+        /*Set the coordinates*/
+        data->point.x = point.x;
+        data->point.y = point.y;
+
+       Serial.printf("Touch point: x %d, y %d\n", point.x, point.y);
+    }
+}
+#endif
+
+void lvgl_port_lock(int timeout_ms)
+{
+    const TickType_t timeout_ticks = (timeout_ms < 0) ? portMAX_DELAY : pdMS_TO_TICKS(timeout_ms);
+    xSemaphoreTakeRecursive(lvgl_mux, timeout_ticks);
+}
+
+void lvgl_port_unlock(void)
+{
+    xSemaphoreGiveRecursive(lvgl_mux);
+}
+
+void lvgl_port_task(void *arg)
+{
+    Serial.println("Starting LVGL task");
+
+    uint32_t task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
+
+    while (1) {
+        // Lock the mutex due to the LVGL APIs are not thread-safe
+        lvgl_port_lock(-1);
+        task_delay_ms = lv_timer_handler();
+        // Release the mutex
+        lvgl_port_unlock();
+        if (task_delay_ms > LVGL_TASK_MAX_DELAY_MS) {
+            task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
+        } else if (task_delay_ms < LVGL_TASK_MIN_DELAY_MS) {
+            task_delay_ms = LVGL_TASK_MIN_DELAY_MS;
+        }
+        vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
+    }
+}
+void hal_setup()
+{
+    panel = new ESP_Panel();
+
+    /* Initialize LVGL core */
+    lv_init();
+
+    /* Initialize LVGL buffers */
+    static lv_disp_draw_buf_t draw_buf;
+    /* Using double buffers is more faster than single buffer */
+    /* Using internal SRAM is more fast than PSRAM (Note: Memory allocated using `malloc` may be located in PSRAM.) */
+    uint8_t *buf = (uint8_t *)heap_caps_calloc(1, LVGL_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_INTERNAL);
+    assert(buf);
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, LVGL_BUF_SIZE);
+
+    /* Initialize the display device */
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = ESP_PANEL_LCD_H_RES;
+    disp_drv.ver_res = ESP_PANEL_LCD_V_RES;
+    disp_drv.flush_cb = lvgl_port_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+#if ESP_PANEL_USE_LCD_TOUCH
+    /* Initialize the input device */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = lvgl_port_tp_read;
+    lv_indev_drv_register(&indev_drv);
+#endif
+
+#if defined(ESP_PANEL_BOARD_ESP32_S3_LCD_EV_BOARD) || defined(ESP_PANEL_BOARD_ESP32_S3_KORVO_2)
+    /**
+     * These development boards require the use of an IO expander to configure the screen,
+     * so it needs to be initialized in advance and registered with the panel for use.
+     *
+     */
+    Serial.println("Initialize IO expander");
+    /* Initialize IO expander */
+    ESP_IOExpander *expander = new ESP_IOExpander_TCA95xx_8bit(ESP_PANEL_LCD_TOUCH_BUS_HOST_ID, ESP_IO_EXPANDER_I2C_TCA9554_ADDRESS_000, ESP_PANEL_LCD_TOUCH_I2C_IO_SCL, ESP_PANEL_LCD_TOUCH_I2C_IO_SDA);
+    expander->init();
+    expander->begin();
+    /* Add into panel */
+    panel->addIOExpander(expander);
+#endif
+
+    /* Initialize bus and device of panel */
+    panel->init();
+#if ESP_PANEL_LCD_BUS_TYPE != ESP_PANEL_BUS_TYPE_RGB
+    /* Register a function to notify LVGL when the panel is ready to flush */
+    /* This is useful for refreshing the screen using DMA transfers */
+    panel->getLcd()->setCallback(notify_lvgl_flush_ready, &disp_drv);
+#endif
+    /* Start panel */
+    panel->begin();
+
+    /* Create a task to run the LVGL task periodically */
+    lvgl_mux = xSemaphoreCreateRecursiveMutex();
+
+    /* Lock the mutex due to the LVGL APIs are not thread-safe */
+    lvgl_port_lock(-1);
+
+    ui_init();
+    ui_gif_init();
+    //ui_timer_init();
+
+    lvgl_port_unlock();
+
+    xTaskCreate(lvgl_port_task, "lvgl", LVGL_TASK_STACK_SIZE, NULL, LVGL_TASK_PRIORITY, NULL);
+
+}
+
+void hal_loop()
+{
+    sleep(1);
+
+}
